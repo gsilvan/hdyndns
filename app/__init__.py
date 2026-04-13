274ac410d43776ba5f4994bc33c923684a07778b
Silvan Gümüsdere <silvan@trollbox.org>
2026-04-13 15:30:01 +0200

    fix: records don't update

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,6 @@
-import typing
-
+from fastapi import FastAPI, Response
 from hcloud import Client
-from hcloud.zones import Zone, ZoneRRSet, ZoneRecord
-from fastapi import FastAPI
-from starlette.responses import Response
-from starlette.status import HTTP_200_OK
+from hcloud.zones import Zone, ZoneRecord, ZoneRRSet
 
 app = FastAPI()
 
@@ -15,38 +11,19 @@ def update(
     host: str,
     password: str,
     ip: str,
-    ipv6: typing.Optional[str] = None,
+    ipv6: str,
 ):
     client = Client(token=password)
-    types = ["A"]
-    if ipv6:
-        types.append("AAAA")
-    for type in types:
-        create_rrset = False
-        try:
-            client.zones.get_rrset(zone=Zone(name=zone), name=host, type=type)
-        except Exception as e:
-            create_rrset = True
-        if create_rrset:
-            response = client.zones.create_rrset(
+    records = [("A", ip), ("AAAA", ipv6)]
+    for record_type, ip in records:
+        client.zones.set_rrset_records(
+            rrset=ZoneRRSet(
                 zone=Zone(name=zone),
                 name=host,
-                type=type,
-                ttl=60,
-                records=[ZoneRecord(value=ip if type == "A" else ipv6)],
-            )
-            response.action.wait_until_finished()
-        try:
-            client.zones.update_rrset_records(
-                rrset=ZoneRRSet(
-                    zone=Zone(name=zone),
-                    name=host,
-                    type=type,
-                ),
-                records=[
-                    ZoneRecord(value=ip),
-                ],
-            )
-        except Exception as e:
-            pass
-    return Response(status_code=HTTP_200_OK)
+                type=record_type,
+            ),
+            records=[
+                ZoneRecord(value=ip),
+            ],
+        )
+    return Response(status_code=200)
